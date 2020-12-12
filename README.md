f803670114ae4fab5c823064f15e1c102e8a3131
Josh Jacobsen <josh.jacobsen@myfoodbag.co.nz>
2020-12-12 17:34:26 +1300

    Finish basic layout

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app), using the [Redux](https://redux.js.org/) and [Redux Toolkit](https://redux-toolkit.js.org/) template.
 
+
+### Notes 
+* Add tests 
+* Do the write up of what needs to be done 
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.js
+++ src/App.js
@@ -3,20 +3,12 @@ import Calendar from 'react-calendar';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Figure from 'react-bootstrap/Figure'
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import sun_clouds from './sun_clouds.png'
 import 'react-calendar/dist/Calendar.css';
 import './App.scss';
-
-const styles = {
-    paddingTop: {
-        paddingTop: "10vh"
-  },
-    borderSolid: {
-    border:'2px solid #000000',
-  }
-}
 
 
 function App() {
@@ -24,7 +16,7 @@ function App() {
   return (
     <div className="App">
       <Container className="containerStyle" >
-        <Row className="textAlignLeft">
+        <Row className="titleRow">
           <Col>
             <Row>
               <p>
@@ -43,19 +35,93 @@ function App() {
           </Col>
         </Row>
         <Row>
-          <Col md={ 4} className="textAlignLeft">
-            This is the weather part of the app
+          <Col md={4} className="weatherPanel">
+            <Row className="weatherPanelRowMargin">
+              {/* This is the weather part of the app */}
+                          <Col>
+                <Figure>
+                  <Figure.Image
+                    width={50}
+                    height={50}
+                    alt="171x180"
+                    src={ sun_clouds}
+                  />
+            </Figure>
+            <p>5 degrees C</p>
+            <p>Clouds and Sunshine</p>
+                {/* <Image src={ sun_clouds} roundedCircle /> */}
+            </Col>
+            </Row> 
+            <Row>
+              <Col>
+                17 January
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                Saturday
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Row>
+                  <Col md={4} sm={4} xs={4} className="textAlignRight">
+                    Img
+                  </Col>
+                  <Col className="textAlignLeft">
+                    Thing to do
+                  </Col>
+                </Row>
+                <Row>
+                  <Col md={4} sm={4} xs={4} className="textAlignRight">
+                    Img
+                  </Col>
+                  <Col className="textAlignLeft">
+                    Thing to do
+                  </Col>
+                </Row>
+                <Row>
+                  <Col md={4} sm={4} xs={4} className="textAlignRight">
+                    Img
+                  </Col>
+                  <Col className="textAlignLeft">
+                    Thing to do
+                  </Col>
+                </Row>
+                <Row>
+                  <Col md={4} sm={4} xs={4} className="textAlignRight">
+                    Img
+                  </Col>
+                  <Col className="textAlignLeft">
+                    Thing to do
+                  </Col>
+                </Row>
+                <Row>
+                  <Col md={4} sm={4} xs={4} className="textAlignRight">
+                    Img
+                  </Col>
+                  <Col className="textAlignLeft">
+                    Thing to do
+                  </Col>
+                </Row>
+            </Col>
+            </Row>
             </Col>
           <Col md={8}>
             <Row className="justify-content-center">
               <Calendar
+                className="calendar"
                 onChange={onChange}
                 value={value}
               />
             </Row>
-            <Row className="justify-content-center">
-              This is where the completed tasks go
-              </Row>
+            <Row className="justify-content-center taskRow">
+              <Col ><p>Completed</p><p>48</p></Col>
+              <Col ><p>Postponed</p><p>12</p></Col>
+              <Col ><p>Undone</p><p>6</p></Col>
+              <Col ><p>All Tasks</p><p>66</p></Col>
+              <Col >+</Col>
+            </Row>
           </Col>
       </Row>
       </Container>
